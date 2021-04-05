126c326dc3eaa18f196fdbf545d687a25c71c8ec
Bertrik Sikken <bertrik@sikken.nl>
2021-04-06 00:52:18 +0200

    Add post-build script that copies the firmware.bin to a file with a target-specific name.

---
+++ Esp32PmSensor/build_script.py
@@ -0,0 +1,10 @@
+Import("env", "projenv")
+import shutil
+
+# copies the firmware.bin file to a file with a target-specific name
+def copy_firmware_bin(source, target, env):
+    binfile_name = f'{env["PROJECT_DIR"]}/{env["PIOENV"]}.bin'
+    shutil.copy(target[0].path, binfile_name)
+
+env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", copy_firmware_bin)
+
